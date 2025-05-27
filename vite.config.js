00e0654e734b00276262c64923bf5c36ad1e177a
whorsyd <rasyid8nuruddin@gmail.com>
2025-05-27 16:53:41 +0700

    adj server for domain

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server:{
-     host: '0.0.0.0', 
-    port: process.env.PORT || 40000 
-  }
-})
+  server: {
+    host: "0.0.0.0",
+    port: process.env.PORT || 40000,
+    hmr: {
+      clientPort: 443, 
+    },
+    allowedHosts: [
+      "seenau-web-fe.onrender.com",
+    ],
+  },
+});
