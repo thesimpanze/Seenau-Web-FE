ce9c469e65aede35c135eb687fceb50924f72fa6
whorsyd <rasyid8nuruddin@gmail.com>
2025-05-27 18:14:09 +0700

    fixing bug

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -67,7 +67,7 @@ const Dashboard = () => {
   return (
     <div className="min-h-screen bg-white pb-24 relative">
       <Navbar />
-      {!isAuth && (<AuthModal/>)}
+      {!isAuth.isAuth && (<AuthModal/>)}
       <section className="bg-black text-white px-6 pb-6 pt-14 flex justify-between items-center">
         <div className="flex items-center gap-4">
           <div className="w-20 h-20 rounded-full bg-gray-200 flex items-center justify-center text-gray-600 text-2xl font-semibold">Ur</div>
