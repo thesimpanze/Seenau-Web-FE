3be4db4ae46049b8556fd71dfc948b4a9b65bf97
whorsyd <rasyid8nuruddin@gmail.com>
2025-05-28 13:58:35 +0700

    fixing bug lagi

--- src/services/UseAuthCheck.jsx
+++ src/services/UseAuthCheck.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { getToken } from "./API";
+import axios from "axios";
 
 const UseAuthCheck = () => {
   const [isAuth, setIsAuth] = useState(false);
